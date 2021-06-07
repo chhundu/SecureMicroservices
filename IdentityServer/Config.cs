4a82d7d2dc5ef865692112f00707c1093c3dc7a6
Chhundu Dorji <chundudorji2018@gmail.com>
2021-06-07 12:09:27 +0600

    Add project files.

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,47 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            { };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+           new ApiScope[]
+           {
+           };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+          new ApiResource[]
+          {
+          };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+          new IdentityResource[]
+          {
+          };
+
+        public static List<TestUser> TestUsers =>
+           new List<TestUser>
+           {
+                /*new TestUser
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "mehmet",
+                    Password = "swn",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "mehmet"),
+                        new Claim(JwtClaimTypes.FamilyName, "ozkaya")
+                    }
+                }*/
+           };
+    }
+}
